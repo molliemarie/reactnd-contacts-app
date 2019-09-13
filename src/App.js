8c7a0c27e27a087e53ac246222f7bbf52f623672
molliemarie <mollie.marie@gmail.com>
2019-09-13 12:45:05 -0700

    moved contacts into state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import ListContacts from "./ListContacts";
 
-const contacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg"
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg"
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg"
+class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg"
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg"
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <ListContacts contacts={this.state.contacts} />
+      </div>
+    );
   }
-];
-
-function App() {
-  return (
-    <div className="App">
-      <ListContacts contacts={contacts} />
-    </div>
-  );
 }
 
 export default App;
