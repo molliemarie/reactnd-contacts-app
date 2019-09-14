f2c56e007a931cff455cde60109cbb1b7654513d
molliemarie <mollie.marie@gmail.com>
2019-09-13 17:24:16 -0700

    changed ListContacts from stateless functional component to class component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,36 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-function ListContacts(props) {
-  return (
-    <ol className="contact-list">
-      {props.contacts.map(contact => (
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}
-          />
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.handle}</p>
-          </div>
-          <button
-            onClick={() => props.onDeleteContact(contact)}
-            className="contact-remove"
-          >
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
-  );
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  };
+  render() {
+    return (
+      <ol className="contact-list">
+        {props.contacts.map(contact => (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+            />
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button
+              onClick={() => props.onDeleteContact(contact)}
+              className="contact-remove"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    );
+  }
 }
-
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
-};
 
 export default ListContacts;
