5bc78bdda5b7e6860333045b175887c7990bb3c5
molliemarie <mollie.marie@gmail.com>
2019-09-23 11:40:37 -0700

    Use componentDidMount to fetch contacts from the server

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
+import * as ContactsAPI from "./utils/ContactsAPI";
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: "karen",
-        name: "Karen Isgrigg",
-        handle: "karen_isgrigg",
-        avatarURL: "http://localhost:5001/karen.jpg"
-      },
-      {
-        id: "richard",
-        name: "Richard Kalehoff",
-        handle: "richardkalehoff",
-        avatarURL: "http://localhost:5001/richard.jpg"
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        handle: "tylermcginnis",
-        avatarURL: "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   };
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState(() => ({
+        contacts
+      }));
+    });
+  }
 
   // removeContact component needs to live here rather than the ListContacts component because this is where the data is.
   removeContact = contact => {
